55cce9f5d2be827c824ef7d9c514beaffb367d7f
ETEC Adolpho Berezin <meu-email@meu-servidor.com>
2024-03-15 22:27:01 -0300

    feito

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+ Console.WriteLine("Soletrando\n");
+ Console.Write("Digite o texto a ser soletrado: ");
+
+ string texto = Console.ReadLine()!;
+
+ Console.WriteLine("\nsoletrando ficaria...\n");
+
+ Console.WriteLine(String.Join("-", texto.ToUpper().ToCharArray()));
+ 
+ Console.WriteLine();
